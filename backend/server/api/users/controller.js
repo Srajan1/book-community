111a50df1b036ce461163f804303604eaced9b80
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-10-04 22:26:18 +0530

    Added sign up route

--- backend/server/api/users/controller.js
+++ backend/server/api/users/controller.js
@@ -5,9 +5,7 @@ const message = require('./message');
 exports.signUp = async function (req, res) {
     const data = req.body;
     try{
-        const user = await db.User.create(data);
         
-        res.status(200).send(apiResponse(1, message.USER_CREATED, {}));
     }catch(err){
         console.log(err);
         res.status(500).send(apiResponse(0, message.INTERNAL_ERROR, {error: err}))
