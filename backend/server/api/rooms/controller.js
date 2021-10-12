065a8fd831471574c066695dc81d0a1722f27e65
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-10-12 12:47:09 +0530

    Rooms for a particular book can be found now

--- backend/server/api/rooms/controller.js
+++ backend/server/api/rooms/controller.js
@@ -1,8 +1,10 @@
 const db = require("../../../models/index");
-const {sequelize} = require("../../../models/index");
+const { sequelize } = require("../../../models/index");
 const { apiResponse } = require("../../helper/apiResponse");
 const message = require("./message");
 const http = require("https");
+const Sequelize = require("Sequelize");
+const Op = Sequelize.Op;
 const userInfo = require("../../helper/userInfo");
 
 exports.create = async function (req, res) {
@@ -18,13 +20,11 @@ exports.create = async function (req, res) {
         resp.on("end", async () => {
           const bookData = JSON.parse(data);
           if (bookData.totalItems === 0) {
-            res
-              .status(404)
-              .send(
-                apiResponse(0, message.BOOK_NOT_FOUND, {
-                  error: "No books match that ISBN number",
-                })
-              );
+            res.status(404).send(
+              apiResponse(0, message.API_NOT_FOUND, {
+                error: "No books match that ISBN number",
+              })
+            );
           } else {
             const transactionInstance = await sequelize.transaction();
             const book = bookData.items[0];
@@ -35,28 +35,33 @@ exports.create = async function (req, res) {
               const { description } = book.volumeInfo;
               const { thumbnail } = book.volumeInfo.imageLinks;
 
-              const newBook = await db.Book.create({
-                title,
-                isbn,
-                subtitle,
-                description,
-                thumbnail,
-              },
-              { transaction: transactionInstance }
+              const newBook = await db.Book.create(
+                {
+                  title,
+                  isbn,
+                  subtitle,
+                  description,
+                  thumbnail,
+                },
+                { transaction: transactionInstance }
               );
               const bookId = newBook.dataValues.id;
               const userId = user.id;
-              const Room = await db.Room.create({bookId, adminId: userId}, { transaction: transactionInstance });
+              const Room = await db.Room.create(
+                { bookId, adminId: userId },
+                { transaction: transactionInstance }
+              );
               transactionInstance.commit();
-              res.status(200)
-              .send(apiResponse(1, message.ROOM_CREATED, {Room}));
+              res
+                .status(200)
+                .send(apiResponse(1, message.ROOM_CREATED, { Room }));
             } catch (err) {
               transactionInstance.rollback();
-              res
-                .status(500)
-                .send(
-                  apiResponse(0, message.INTERNAL_ERROR, { error: err.errors[0].message })
-                );
+              res.status(500).send(
+                apiResponse(0, message.INTERNAL_ERROR, {
+                  error: err.errors[0].message,
+                })
+              );
             }
           }
         });
@@ -67,4 +72,56 @@ exports.create = async function (req, res) {
         .status(500)
         .send(apiResponse(0, message.INTERNAL_ERROR, { error: err.message }));
     });
+};
+
+exports.index = async function (req, res) {
+  const { title, isbn } = req.query;
+  if (isbn) {
+    try {
+      const where = {};
+      where.isbn = isbn;
+      const room = await db.Room.findAll({
+        include: [{ model: db.Book, where: where }],
+      });
+      if (room) {
+        res
+          .status(404)
+          .send(apiResponse(0, message.ROOM_EXISTS, { Room: room }));
+      } else {
+        res.status(404).send(apiResponse(0, message.BOOK_NOT_FOUND, {}));
+      }
+    } catch (err) {
+      res
+        .status(500)
+        .send(apiResponse(0, message.INTERNAL_ERROR, { error: err.message }));
+    }
+  } else if (title) {
+    try {
+      const where = {};
+      where.title = { [Op.like]: "%" + title + "%" };
+      const room = await db.Room.findAll({
+        include: [{ model: db.Book, where: where }],
+      });
+      if (room) {
+        res
+          .status(404)
+          .send(apiResponse(0, message.ROOM_EXISTS, { Room: room }));
+      } else {
+        res.status(404).send(apiResponse(0, message.BOOK_NOT_FOUND, {}));
+      }
+    } catch (err) {
+      res
+        .status(500)
+        .send(apiResponse(0, message.INTERNAL_ERROR, { error: err.message }));
+    }
+  } else {
+    const room = await db.Room.findAll({
+      include: [{ model: db.Book }],
+    });
+    if (room) {
+      res.status(404).send(apiResponse(0, message.ROOM_EXISTS, { Room: room }));
+    } else {
+      res.status(404).send(apiResponse(0, message.BOOK_NOT_FOUND, {}));
+    }
+  }
 };
--- backend/server/api/rooms/route.js
+++ backend/server/api/rooms/route.js
@@ -1,6 +1,8 @@
 const controller = require('./controller');
 const {validate} = require('../../middleware/validate')
 const validation = require('./validation');
+const {authorization} = require('../../middleware/authorization')
 module.exports = (router) => {
-    router.post('/room', validate(validation.create), controller.create);
+    router.post('/room', authorization, validate(validation.create), controller.create);
+    router.get('/room', authorization, controller.index);
 }
