a315110de1bfa386d19e7b897fd4591957c68ebc
Srajan Kishor Gupta <srakishor.g@gmail.com>
2022-02-05 16:32:58 +0530

    Drawer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createTheme, ThemeProvider, Typography } from "@mui/material";
+
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Layout from "./components/drawer";
+import { red } from '@mui/material/colors';
+
+const color = red;
+const theme = createTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#0431E9',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      // light: '#0066ff',
+      main: '#5979FC',
+      // dark: will be calculated from palette.secondary.main,
+      // contrastText: '#ffcc00',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>        
+          <Layout>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          </Layout>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Typography from '@mui/material/Typography'
+
+export default function Home() {
+    {console.log('Hello');}
+  return (
+      <div className="">
+          <Typography variant="h6" color="initial">Hello</Typography>
+      </div>
+  );
+}
